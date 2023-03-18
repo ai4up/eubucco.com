e88db9524d052ab020d30785c9c44fc691b3c016
GezaBoi <geza.brueser@gmail.com>
2023-03-18 10:32:31 +0100

    unified tutorial template

--- eubucco/tutorials/views.py
+++ eubucco/tutorials/views.py
@@ -1,13 +1,16 @@
 from django.shortcuts import render
+from django.templatetags.static import static
 from django.views.decorators.cache import cache_page
 
 
 # Create your views here.
 @cache_page(60 * 60)
 def getting_started(request):
-    return render(request, "tutorials/getting_started.html")
+    url = static("html/tutorial-getting-started.html")
+    return render(request, "tutorials/tutorial.html", {"url": url})
 
 
 @cache_page(60 * 60)
 def predicting_age(request):
-    return render(request, "tutorials/predicting_age.html")
+    url = static("html/tutorial-predicting-building-age.html")
+    return render(request, "tutorials/tutorial.html", {"url": url})
