6cc63b09b1c11300ae2b6bfa073d90bd53fdc1fa
GezaBoi <geza.brueser@gmail.com>
2023-03-19 13:22:42 +0000

    dark/light mode support in tutorials

--- eubucco/tutorials/views.py
+++ eubucco/tutorials/views.py
@@ -6,11 +6,19 @@ from django.views.decorators.cache import cache_page
 # Create your views here.
 @cache_page(60 * 60)
 def getting_started(request):
-    url = static("html/tutorial-getting-started.html")
-    return render(request, "tutorials/tutorial.html", {"url": url})
+    light_url = static("html/tutorial-getting-started.html")
+    return render(
+        request,
+        "tutorials/tutorial.html",
+        {"light_url": light_url, "dark_url": light_url},
+    )
 
 
 @cache_page(60 * 60)
 def predicting_age(request):
-    url = static("html/tutorial-predicting-building-age.html")
-    return render(request, "tutorials/tutorial.html", {"url": url})
+    light_url = static("html/tutorial-predicting-building-age.html")
+    return render(
+        request,
+        "tutorials/tutorial.html",
+        {"light_url": light_url, "dark_url": light_url},
+    )
