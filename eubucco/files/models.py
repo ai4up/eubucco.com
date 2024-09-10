b19c0ea095d0000f0b296f487bf3875d06fcf182
GezaBoi <dev@geza.io>
2024-09-10 15:28:31 +0200

    Revert "added version to files table"
    This reverts commit c9d52739d3b432abe93d6ef7bae434b3ed993cc2.

--- eubucco/files/models.py
+++ eubucco/files/models.py
@@ -3,8 +3,6 @@ import uuid
 from django.db import models
 from django.utils.translation import gettext_lazy as _
 
-from eubucco.utils.version_enum import VersionEnum
-
 
 # Create your models here.
 class FileType(models.TextChoices):
@@ -21,7 +19,6 @@ class File(models.Model):
     path = models.FilePathField(max_length=200, unique=True)
     type = models.CharField(max_length=2, choices=FileType.choices, db_index=True)
     info = models.CharField(max_length=200, null=False, blank=True)
-    version = models.IntegerField(choices=VersionEnum.choices, null=False)
 
     def __str__(self):
         return self.name
