815f7d077074a743db3ec1979d0c7dc2fc381301
Florian Nachtigall <nachtigall.florian@gmail.com>
2026-01-27 19:09:12 +0100

    redirect /data/ root to /files/ index
    Redirects the base /data/ path to the new /files/ endpoint to maintain
    backwards compatibility for legacy bookmarks. The /data/map endpoint
    remains active and unaffected.

--- eubucco/data/urls.py
+++ eubucco/data/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
+from django.shortcuts import redirect
 
 from . import views
 
 app_name = "data"
 urlpatterns = [
+    path("", lambda r: redirect("files:index"), name="index"),
     path("map", views.map, name="map"),
 ]
